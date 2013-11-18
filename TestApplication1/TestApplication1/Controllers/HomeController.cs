7973ff3629654a3fe08caeffa0df3387c3be614d
yankelevich <alexey.yankelevich@gmail.com>
2013-11-18 12:35:54 +0200

    Home5

---
+++ TestApplication1/TestApplication1/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace TestApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "It's my HW5";
+        }
+    }
+}
